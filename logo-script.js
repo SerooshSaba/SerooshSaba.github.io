250eefa815eff38bf395705f5582f7cf9235e8ae
Seroosh Saba <serooshs@hiof.no>
2022-07-05 13:35:59 +0200

    Update logo-script.js

--- logo-script.js
+++ logo-script.js
@@ -398,14 +398,14 @@ setInterval(() => {
     box2.centerRot( 0.5, 0.4, 0 );
     box3.centerRot( 0.5, 0.4, 0 );
     project(box3, 0.75, ctx1 );
-    project(box2, 1, ctx1 );
-    project(box1, 3, ctx1 );
+    project(box2, 1,    ctx1 );
+    project(box1, 3,    ctx1 );
     
     // Rander spheres to second canvas
     ctx2.clearRect(0, 0, cubeCanvas.width, cubeCanvas.height);
     project( sphere1, 0.2,  ctx2 );
     project( sphere2, 0.5,  ctx2 );
-    project( sphere3, 1, ctx2 );
+    project( sphere3, 1,    ctx2 );
     sphere1.centerRot( 0.5, -0.4, 0 );
     sphere2.centerRot( 0.5, -0.4, 0 );
     sphere3.centerRot( 0.5, -0.4, 0 );
